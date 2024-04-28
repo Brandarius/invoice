ed1811e570a9d7b7bb5f7eba791bf01e3bd15eb6
Bran <54346677+Brandarius@users.noreply.github.com>
2024-04-28 17:55:46 -0500

    errors i gotta fix

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+'''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///customers.db'  # SQLite database
+db = SQLAlchemy(app)
 
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    # Add more columns as needed (e.g., address, email, etc.)
+
+    def __repr__(self):
+        return '<Customer %r>' % self.name
+    '''
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
-
+'''
 @app.route('/customers')
 def customers():
     return render_template('customers.html')
-
+'''
 if __name__ == '__main__':
     app.run(debug=True)
