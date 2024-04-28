302832d716c7a344a4a5bbb6fc4ce4e243987a14
Bran <54346677+Brandarius@users.noreply.github.com>
2024-04-28 16:36:06 -0500

    added customers page

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def main_page():
     return render_template('main_page.html')
 
+@app.route('/customers')
+def customers():
+    return render_template('customers.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
