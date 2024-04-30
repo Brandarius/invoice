bfb1427dcebb0bd2e073d43fc3bef2c4c3ea528c
Bran <54346677+Brandarius@users.noreply.github.com>
2024-04-30 14:37:15 -0500

    cleaned up my code a little bit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///customers.db'
 db = SQLAlchemy(app)
 
-# Define the Customer model
+#Database Creation and Bull Crap 
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
@@ -13,24 +13,18 @@ class Customer(db.Model):
     email_address = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(100), nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
-
     def __repr__(self):
         return f'<Customer {self.id}: {self.first_name} {self.last_name}>'
 
-# Create the database tables
 with app.app_context():
     db.create_all()
-
-
-@app.route('/')
-def main_page():
-    return render_template('main_page.html')
 
 @app.route('/customers')
 def list_customers():
     customers = Customer.query.all()
     return render_template('customers.html', customers=customers)
 
+# Function to Add Customers
 @app.route('/add_customer', methods=['POST'])
 def add_customer():
     new_customer = Customer(
@@ -44,19 +38,26 @@ def add_customer():
     db.session.commit()
     return redirect(url_for('list_customers'))
 
+#Function to delete Customers(Still need to add confirmation window)
+@app.route('/delete_customer/<int:customer_id>', methods=['POST'])
+def delete_customer(customer_id):
+    customer = Customer.query.get_or_404(customer_id)
+    db.session.delete(customer)
+    db.session.commit()
+    return redirect(url_for('list_customers'))
 
 
+#The Main Page with the invoice creation that hasnt been set up yet
+@app.route('/')
+def main_page():
+    return render_template('main_page.html')
+
+
+#Inventory page without the add item function yet
 @app.route('/inventory')
 def add_item():
     return render_template('inventory.html')
 
-@app.route('/delete_customer/<int:customer_id>', methods=['POST'])
-def delete_customer(customer_id):
-    customer = Customer.query.get_or_404(customer_id)
-    # Assuming you're using SQLAlchemy, you can delete the customer directly from the database
-    db.session.delete(customer)
-    db.session.commit()
-    return redirect(url_for('list_customers'))
 
 if __name__ == '__main__':
     app.run(debug=True)
